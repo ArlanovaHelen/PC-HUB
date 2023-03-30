584453b2bbd9d2227a87eee58766647bc4256273
Lena <54290671+ArlanovaHelen@users.noreply.github.com>
2023-03-30 11:58:37 +0300

    Add files via upload

--- lesson8.py
+++ lesson8.py
@@ -10,8 +10,8 @@ my_list = [
 ]
 new_list = []
 for i in range(len(my_list)):
-    if i / 2 == 0:
-        new_list.append(my_list[i][::-1])
+    if i % 2 == 0:
+        new_list.append(str(my_list[i])[::-1])
     else:
         new_list.append(my_list[i])
 print(new_list)
